3739938da00adf1c0c120d85295fb02676cb0e46
AlexeyKondrukevich <kondr08@mail.ru>
2023-06-24 15:59:06 +0400

    Create apps: books, api

--- library/library/settings.py
+++ library/library/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "api",
+    "books",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
