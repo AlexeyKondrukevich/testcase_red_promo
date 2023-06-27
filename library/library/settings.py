9c693ee1cd79a966ecadf944788e71c44424b60f
AlexeyKondrukevich <kondr08@mail.ru>
2023-06-27 21:45:25 +0400

    Create dockerfile

--- library/library/settings.py
+++ library/library/settings.py
@@ -92,8 +92,8 @@ DATABASES = {
         "NAME": os.environ["DB_NAME"],
         "USER": os.environ["POSTGRES_USER"],
         "PASSWORD": os.environ["POSTGRES_PASSWORD"],
-        "HOST": "localhost",
-        "PORT": "",
+        "HOST": os.environ["DB_HOST"],
+        "PORT": os.environ["DB_PORT"],
     }
 }
 
